b1a2944f19d04d9341c83971ad06f347f3f189f7
Nishan <aanishan339@gmail.com>
2026-08-12 12:56:55 +0600

    Created custom backend to handle login process using usernamer or email

--- session/backends.py
+++ session/backends.py
@@ -2,17 +2,19 @@ from django.contrib.auth.backends import ModelBackend
 
 from django.contrib.auth import get_user_model
 
+from django.db.models import Q
+
 User = get_user_model()
 
-class EmailBackend(ModelBackend):
+class CustomBackend(ModelBackend):
 
-    def authenticate(self , request , email = None , password = None , **kwargs):
+    def authenticate(self , request , username = None , password = None , **kwargs):
 
-        if not email:
+        if not username:
             return None
 
         try:
-            user = User.objects.get(email = email)
+            user = User.objects.get(Q(username = username) | Q(email = username))
 
         except User.DoesNotExist:
             return None
