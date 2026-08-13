7794877128aa857db12463db6864d51210e6814c
Nishan <aanishan339@gmail.com>
2026-08-14 00:36:00 +0600

    Included session , product and summernote url

--- foodmart/urls.py
+++ foodmart/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
+
+from product.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/', home , name = "home"),
+    path('summernote/', include("django_summernote.urls")),
+
+    path('session/', include("session.urls")),
+    path('product/', include("product.urls")),
 ]
