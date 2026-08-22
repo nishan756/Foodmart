639c4bf7963e44163b82b47ebd610790ed49173c
Nishan <aanishan339@gmail.com>
2026-08-22 12:59:39 +0600

    Created login , signup , logout , user deletion method

--- session/urls.py
+++ session/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import user_login , user_logout , user_signup , user_delete
 
 urlpatterns = [
-    path("user_signup/" , user_signup , name = "signup"),
-    path("user_login/" , user_login , name = "login"),
-    path("user_logout/" , user_logout , name = "logout"),
-    path("user_delete/" , user_delete , name = "user-delete"),
+    path("user-signup/" , user_signup , name = "signup"),
+    path("user-login/" , user_login , name = "login"),
+    path("user-logout/" , user_logout , name = "logout"),
+    path("user-delete/" , user_delete , name = "user-delete"),
 ]
