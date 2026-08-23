44842f5ccb12e16971b7fd75ffd686b892a72176
Nishan <aanishan339@gmail.com>
2026-08-23 23:47:00 +0600

    Registered CartItem , OrderItem , Order model

--- cart/admin.py
+++ cart/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
-from .models import Cart
+from .models import Cart , CartItem , OrderItem , Order
 
 
 @admin.register(Cart)
 class CartAdmin(admin.ModelAdmin):
     list_display = ["user" , "created_at"]
+    list_per_page = 100
+
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ["cart" , "product" , "qty" , "applicable_price"]
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ["product" , "unit_price" , "order" , "created_at"]
+    list_per_page = 100
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["user" , "total_price" , "status" , "created_at" , "shipped_at"]
     list_per_page = 100
