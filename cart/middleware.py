8e360d4e3da32ea6bd2d7c153181b226d28c9843
Nishan <aanishan339@gmail.com>
2026-08-23 23:43:12 +0600

    Fixed middleware issue. This request.cart is only worls with logged in user

--- cart/middleware.py
+++ cart/middleware.py
@@ -1,28 +1,19 @@
-from .models import Cart
+from .service import CartService
 
 class CartMiddleware:
 
-    def __init__(self , get_response):
+    def __init__(self, get_response):
         self.get_response = get_response
 
-    def __call__(self , request):
+    def __call__(self, request):
+
+        request.cart = None
+
         if request.user.is_authenticated:
-            cart , created = Cart.objects.get_or_create(user = request.user)
+            request.cart = CartService().get_cart(
+                user=request.user
+            )
 
-        else:
-            
-            if not request.session.session_key:
-                request.session.create()
-
-            session_id = request.session.session_key
-            
-            cart , created = Cart.objects.get_or_create(session_id = session_id)
-
-        request.cart = cart
-        
         response = self.get_response(request)
 
         return response
-
-
-            
