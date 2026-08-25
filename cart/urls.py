95a4551652561bbff4574f093eb0d36629392e1a
Nishan <aanishan339@gmail.com>
2026-08-25 20:37:56 +0600

    Added cart/ Order related url

---
+++ cart/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import add_item , del_item , increase_qty , decrease_qty , checkout , confirm_order , my_orders
+
+
+urlpatterns = [
+    path("add/<uuid:id>/" , add_item , name = "add-item"),
+    path("delete/<uuid:id>/" , del_item , name = "del-item"),
+    path("increase-qty/<uuid:id>/" , increase_qty , name = "increase-qty"),
+    path("decrease-qty/<uuid:id>/" , decrease_qty , name = "decrease-qty"),
+    path("checkout/" , checkout , name = "checkout"),
+    path("confirm-order/" , confirm_order , name = "confirm-order"),
+    path("my-orders/" , my_orders , name = "my-orders"),
+]
