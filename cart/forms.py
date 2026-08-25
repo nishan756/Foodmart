b8ed08e3162df909ab15855126ba6f40e850437e
Nishan <aanishan339@gmail.com>
2026-08-25 20:36:44 +0600

    Created OrderFilterForm

--- cart/forms.py
+++ cart/forms.py
@@ -3,16 +3,32 @@ from django import forms
 from .models import Order
 
 class OrderForm(forms.ModelForm):
+    def __init__(self , *args , **kwargs):
+        super().__init__(*args , **kwargs)
+    
+        for name , field in self.fields.items():
+    
+            field.widget.attrs.update({"class":"form-control" , "placeholder":name.replace("_" , " ").title()})
+    
+            field.label_suffix = ""
+            
     class Meta:
         model = Order
         exclude = ["user" , "total_price" , "status" , "created_at" , "shipped_at"]
 
+class OrderFilterForm(forms.Form):
+    date_from = forms.DateField(
+        widget = forms.DateInput(attrs = {"type":"date"}),
+    )
+    date_to = forms.DateField(
+        widget = forms.DateInput(attrs = {"type":"date"})
+    )
 
-    def __init__(self , *args , **kwargs):
+    def __init__(self, *args , **kwargs):
         super().__init__(*args , **kwargs)
 
         for name , field in self.fields.items():
-
-            field.widget.attrs.update({"class":"form-control" , "placeholder":name.replace("_" , " ").title() , "disabled":True if name == "full_name" or name == "email" else False , "id":name})
-
+            field.widget.attrs.update({"class":"form-control" , "id":name})
             field.label_suffix = ""
+            print(name)
+
