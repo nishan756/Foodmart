9a5f4ef688bcad09e05ee0ea1864e436b5541ee6
Nishan <aanishan339@gmail.com>
2026-08-30 13:09:44 +0600

    Created blog repo

---
+++ blog/repository.py
@@ -0,0 +1,19 @@
+from .models import Blog
+from django.core.exceptions import ObjectDoesNotExist
+
+class BlogRepo:
+
+    def all_blogs(self , query:dict):
+        blogs = Blog.objects.all()
+
+        if query.get("title"):
+            blogs = blogs.filter(title__icontains = query.get("title"))
+
+        return blogs
+
+    def get_blog_detail(self , slug:str):
+        try:
+            return Blog.objects.get(slug = slug)
+
+        except Blog.DoesNotExist:
+            raise ObjectDoesNotExist("Blog not found")
