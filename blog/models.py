e249abd11be56baf3d082b7589328fe91bbd4e46
Nishan <aanishan339@gmail.com>
2026-08-30 13:09:31 +0600

    Created blog model

---
+++ blog/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django_summernote.fields import SummernoteTextField
+from django.utils.text import slugify
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length = 100)
+
+    slug = models.SlugField(unique = True , blank = True , null = True)
+
+    banner = models.ImageField(upload_to = "blog_image" , blank = True , null = True)
+
+    short_description = models.TextField(max_length = 200)
+
+    description = SummernoteTextField()
+
+    created_at = models.DateTimeField(auto_now_add = True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return self.title
+
+    def save(self , *args , **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args , **kwargs)
