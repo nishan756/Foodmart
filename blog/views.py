2b955f5c2d2c52a575bbd824a762c2a43a0891c3
Nishan <aanishan339@gmail.com>
2026-08-30 13:10:21 +0600

    Created all_blogs and blog_detail view

---
+++ blog/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render , redirect
+from .service import BlogServie
+from django.views.decorators.http import require_GET
+from django.contrib import messages
+from django.core.exceptions import ObjectDoesNotExist
+
+@require_GET
+def all_blogs(request):
+
+    query = request.GET.dict()
+
+    page = query.pop("page" , 1)
+
+    per_page = query.pop("per_page" , 100)
+
+    try:page = int(page)
+    except(ValueError , TypeError):page = 1
+
+    try:per_page = int(per_page)
+    except(ValueError , TypeError):per_page = 100
+
+    try:blogs = BlogServie().all_blogs(page , per_page , query);return render(request , "all-blogs.html" , {"blogs":blogs})
+    except Exception:messages.error(request , "Something went wrong in this page. Try again later")
+
+    return redirect("all-products")
+
+@require_GET
+def blog_detail(request , slug):
+
+    
+    try:blog = BlogServie().get_blog_detail(slug);return render(request , "blog-detail.html" , {"blog":blog})
+    except ObjectDoesNotExist as e:messages.info(request , str(e))
+
+    return redirect("all-blogs")
