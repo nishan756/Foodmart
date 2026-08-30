a392ad28f350e108f50c7926292cb50a661fbb0d
Nishan <aanishan339@gmail.com>
2026-08-30 19:44:04 +0600

    Registered BlogTag model

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Blog
+from .models import Blog , BlogTag
 
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
     list_display = ["title" , "created_at"]
+    list_per_page = 100
+
+@admin.register(BlogTag)
+class BlogTagAdmin(admin.ModelAdmin):
+    list_display = ["title"]
     list_per_page = 100
