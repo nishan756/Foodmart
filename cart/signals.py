4337ca3289d5087862bac0bc7c44b4fcc380c4bc
Nishan <aanishan339@gmail.com>
2026-09-01 17:35:33 +0600

    Added send_email method to send email if order created or shipped

--- cart/signals.py
+++ cart/signals.py
@@ -1,7 +1,9 @@
 from django.db.models.signals import post_save
 from django.contrib.auth import get_user_model
-from cart.models import Cart
+from cart.models import Cart , Order
 from django.dispatch import receiver
+from threading import Thread
+from core.services.email import EmailService
 
 User = get_user_model()
 
@@ -10,3 +12,28 @@ def create_cart(sender , instance , created , **kwargs):
 
     if created:
         return Cart.objects.create(user = instance)
+
+
+@receiver(signal = post_save , sender = Order)
+def send_email(sender , instance , created , **kwargs):
+
+    kwargs = {
+        "email_to":[instance.user.email],
+        "context":{
+            "instance":instance,
+            "user":instance.user,
+        }
+    }
+
+    if created:
+        kwargs["subject"] = "Thanks for your order."
+        kwargs["template_name"] = "order-completed.html"
+
+    else:
+        if instance.status == "shipped":
+            kwargs["subject"] = "Your order has reached."
+            kwargs["template_name"] = "order-shipped.html"
+
+    thread = Thread(target = EmailService.send_email , kwargs = kwargs)
+
+    thread.start()
