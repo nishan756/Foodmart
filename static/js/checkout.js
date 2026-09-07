a5c2e363a08f42bc9c26bf5ec374225c7dbf21a5
Nishan <aanishan339@gmail.com>
2026-09-07 22:53:03 +0600

    Created loadThanas and loadShippingCharge function

---
+++ static/js/checkout.js
@@ -0,0 +1,49 @@
+async function loadThanas(){
+    const districtId = document.getElementById("district").value;
+    const thanaSelect = document.getElementById("thana");
+
+    thanaSelect.innerHTML = '<option value="">Loading..</option>';
+
+    try {
+        const response = await fetch(`/site-setting/thana-list/?district_id=${districtId}`);
+        const data = await response.json();
+
+        if(Object.keys(data.thanas).length > 0 ){
+            thanaSelect.innerHTML = '<option value="">Select Thana</option>';
+        }
+        else{
+            thanaSelect.innerHTML = '<option>No Thana Found</option>';
+            return;
+        }
+        data.thanas.forEach(thana => {
+            thanaSelect.innerHTML += `
+                <option value="${thana.id}">${thana.name}</option>
+            `;
+        });
+
+    } catch (error) {
+        console.error(error);
+        thanaSelect.innerHTML = '<option value="">Failed to load</option>';
+    }
+}
+
+async function loadShippingCharge(){
+    const thanaId = document.getElementById("thana").value;
+    const shippingCharge = document.getElementById("shippingCharge");
+    if(thanaId){
+
+        try{
+            const response = await fetch(`/site-setting/shipping-charge/?thana_id=${thanaId}`);
+            const data = await response.json();
+
+            if(data.success){
+                shippingCharge.innerHTML = data.charge
+            }
+            
+        }
+        catch(error){
+            
+        }
+    }
+
+}
