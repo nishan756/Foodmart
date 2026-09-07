a776c66ab61498138c24679a8ad1885443b6ad85
Nishan <aanishan339@gmail.com>
2026-09-08 00:21:09 +0600

    Return JsonResponse

--- wishlist/views.py
+++ wishlist/views.py
@@ -3,6 +3,7 @@ from .service import WishlistService
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST , require_GET
+from django.http import JsonResponse
 
 from core.exceptions import ObjectAlreadyExists
 from django.core.exceptions import ObjectDoesNotExist
@@ -27,31 +28,24 @@ def my_wishlist(request):
 @require_POST
 def add_to_wishlist(request , id):
 
-    HTTP_REFERER = request.META.get('HTTP_REFERER' , "/")
-
-    if not url_has_allowed_host_and_scheme(HTTP_REFERER , request.get_host()):
-        HTTP_REFERER = "all-products"
-
     try:
         wishlist_item = WishlistService().add_to_wishlist(request.user , id)
-        messages.success(request , "Successfully added {} in your wishlist".format(wishlist_item.product.title))
+        return JsonResponse({"message":"Successfully added {} in your wishlist".format(wishlist_item.product.title) , "tags":"success"})
 
     except ObjectAlreadyExists as e:
-        messages.info(request , str(e))
+        return JsonResponse({"message":str(e) , "tags":"info"})
 
     except ObjectDoesNotExist as e:
-        messages.error(request , str(e))
+        return JsonResponse({"message":str(e) , "tags":"info"})
 
-    return redirect(HTTP_REFERER)
 
 @login_required(login_url = "login")
 @require_POST
 def delete_from_wishlist(request , id):
     try:
         WishlistService().delete_from_wishlist(request.user , id)
-        messages.success(request , "Successfully removed wishlist item")
+        return JsonResponse({"message":"Successfully removed wishlist item" , "tags":"success" , "status":200})
     
     except ObjectDoesNotExist as e:
-        messages.error(request , str(e))
+        return JsonResponse({"message":str(e) , "tags":"info"})
     
-    return redirect("my-wishlist")
