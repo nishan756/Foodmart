26c79fa85c0cc794ea669a7e6deead42ae07d7ce
Nishan <aanishan339@gmail.com>
2026-09-08 12:18:36 +0600

    Added site_setting.context_processor.get_site_info in list

--- foodmart/settings.py
+++ foodmart/settings.py
@@ -77,6 +77,7 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'cart.context_processor.cart_detail',
                 'product.context_processor.categories',
+                'site_setting.context_processor.get_site_info',
             ],
         },
     },
