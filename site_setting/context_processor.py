79af1739653fbb19c56f59001311c5e12126accb
Nishan <aanishan339@gmail.com>
2026-09-09 12:40:35 +0600

    Created get_site_social_link view

--- site_setting/context_processor.py
+++ site_setting/context_processor.py
@@ -1,7 +1,13 @@
-from .models import SiteInfo
+from .models import SiteInfo , SocialLink
 
 def get_site_info(request):
 
     site_info = SiteInfo.objects.first()
 
     return {"site_info":site_info}
+
+def get_site_social_links(request):
+
+    social_links = SocialLink.objects.all()
+
+    return {"social_links":social_links}
